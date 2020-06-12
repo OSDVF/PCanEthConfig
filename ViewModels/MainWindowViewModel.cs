f58bd55233ba2851554348d8f5c94e52edd6ab5f
OSDVF <o.s.dv.f@seznam.cz>
2020-06-12 13:41:26 +0200

    Add project files.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EthCanConfig.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace EthCanConfig.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
