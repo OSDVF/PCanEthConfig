f0df39b2efeaee398663a654ff1217cbab1b51c5
OSDVF <o.s.dv.f@seznam.cz>
2020-07-07 14:33:53 +0200

    Added basic treeview

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
+using EthCanConfig.Models;
+using System.Collections.ObjectModel;
 
 namespace EthCanConfig.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Avalonia!";
+        public IPAddress ConnectedDevice;
+        public string ConnectedDeviceIP => ConnectedDevice == null ? "Unavaliable" : ConnectedDevice.ToString();
+        public ObservableCollection<ConfigurationSetting> Settings { get; } = new ObservableCollection<ConfigurationSetting>() {
+            new ConfigurationSetting("logFile","ethCanRouter.log"),
+
+        };
     }
 }
